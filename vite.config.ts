88e3f877836355015e434637d7bd20f7e6561be1
agreage-ai <agreage@moorea.fr>
2026-06-17 17:33:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    target: 'es2020',
-  },
-  build: {
-    target: 'es2020',
-  },
 })
